45ed7c68ea63a5fd38c0c06c934dc04cecc865be
Onee_Only <kimww0306@gmail.com>
2023-02-25 12:31:09 +0900

    jwt access token 갱신 함수 추가

---
+++ src/api/server.ts
@@ -0,0 +1,41 @@
+import axios, { InternalAxiosRequestConfig } from "axios";
+import Cookie from "js-cookie";
+import moment from "moment";
+
+const server = axios.create({
+    baseURL: "https://oneecoin-explorer-server.onrender.com",
+    withCredentials: true,
+});
+
+const refresh = async (
+    config: InternalAxiosRequestConfig
+): Promise<InternalAxiosRequestConfig> => {
+    const refreshToken = Cookie.get("refresh");
+    let exp = localStorage.getItem("exp");
+    let token = localStorage.getItem("accessToken");
+
+    // 토큰이 만료되었고, refreshToken 이 저장되어 있을 때
+    if (moment.unix(Number(exp)).diff(moment().unix()) < 0 && refreshToken) {
+        const body = {
+            refreshToken,
+        };
+
+        // 토큰 갱신 서버통신
+        const { data } = await server.post("/auth/refresh", body);
+
+        token = data.data.access;
+        exp = data.data.exp;
+        localStorage.setItem("accessToken", data.data.accessToken);
+        localStorage.setItem("exp", exp!);
+    }
+
+    config.headers!.Authorization = `Bearer ${token}`;
+
+    return config;
+};
+
+const refreshErrorHandle = (err: any) => {
+    Cookie.remove("refresh");
+};
+
+server.interceptors.request.use(refresh, refreshErrorHandle);
