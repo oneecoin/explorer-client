a47fb5b491d3e04561b1724c8ad86266a1ddb40b
Onee_Only <kimww0306@gmail.com>
2023-03-04 19:19:22 +0900

    block list

--- src/api/mempool/blocks.ts
+++ src/api/mempool/blocks.ts
@@ -1,6 +1,17 @@
+import { QueryFunctionContext } from "@tanstack/react-query";
 import { mempool } from "./mempool";
 
 export const getLatestBlock = async () => {
     const res = await mempool.get("/blocks/summary");
+    return res.data;
+};
+
+export const getBlocks = async ({ queryKey }: QueryFunctionContext) => {
+    const [, page] = queryKey;
+    const res = await mempool.get("/blocks", {
+        params: {
+            page,
+        },
+    });
     return res.data;
 };
--- src/routes/Blocks.tsx
+++ src/routes/Blocks.tsx
@@ -1,35 +1,109 @@
-import { Box, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+    Box,
+    IconButton,
+    Link,
+    Spinner,
+    Table,
+    TableCaption,
+    TableContainer,
+    Tbody,
+    Td,
+    Text,
+    Th,
+    Thead,
+    Tr,
+    useColorModeValue,
+} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { getBlocks } from "../api/mempool/blocks";
+import { IBlockListElem } from "../api/mempool/types";
+import Helmet from "../components/Helmet";
+import { formatTime } from "../lib/format-time";
 
 export default function Blocks() {
+    const highlightColor = useColorModeValue("blue.600", "blue.300");
+    const [page, setPage] = useState(1);
+    const { data, isLoading } = useQuery<IBlockListElem[]>(["blocks", page], getBlocks);
+
     return (
         <>
+            <Helmet title={`Blocks: page ${page}`} />
             <Box
                 marginTop={"12"}
                 display={"flex"}
                 alignItems={"center"}
                 flexDirection={"column"}
             >
-                <Text>Page 1</Text>
-                <Table variant={"striped"} width={"90%"} marginTop={"3"} size={"sm"}>
-                    <Thead>
-                        <Tr width={"100%"}>
-                            <Th>height</Th>
-                            <Th>hash</Th>
-                            <Th>transactions count</Th>
-                            <Th>time stamp</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        <Tr>
-                            <Td minWidth={"32"}>32</Td>
-                            <Td isTruncated maxWidth={"64"}>
-                                eafelfajhfleajfhalkejfefaaefhalfjheflakejfheffaefaefaewfwefewfhalkjfheljfhaefaefaeffefaefwf
-                            </Td>
-                            <Td width={"52"}>3</Td>
-                            <Td>2022-03-01 22:20:10</Td>
-                        </Tr>
-                    </Tbody>
-                </Table>
+                <TableContainer width={"90%"} marginTop={"3"}>
+                    <Table variant={"striped"} size={"sm"}>
+                        <Thead>
+                            <Tr width={"100%"}>
+                                <Th>height</Th>
+                                <Th>hash</Th>
+                                <Th>transactions count</Th>
+                                <Th>time stamp</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {isLoading ? (
+                                <Tr>
+                                    <Td colSpan={4}>
+                                        <Spinner size={"xl"} thickness="5px" />
+                                    </Td>
+                                </Tr>
+                            ) : (
+                                data?.map((block) => (
+                                    <>
+                                        <Tr>
+                                            <Td minWidth={"12"}>{block.height}</Td>
+                                            <Td isTruncated maxWidth={"64"}>
+                                                <Link
+                                                    as={ReactRouterLink}
+                                                    to={`/blocks/${block.hash}`}
+                                                    color={highlightColor}
+                                                >
+                                                    {block.hash}
+                                                </Link>
+                                            </Td>
+                                            <Td width={"52"}>
+                                                {block.transactionsCount}
+                                            </Td>
+                                            <Td>{formatTime(block.timestamp)}</Td>
+                                        </Tr>
+                                    </>
+                                ))
+                            )}
+                        </Tbody>
+                        <TableCaption fontSize={"xl"}>
+                            <IconButton
+                                aria-label="left-icon"
+                                icon={<FaAngleLeft />}
+                                colorScheme={"blue"}
+                                variant={"ghost"}
+                                isDisabled={isLoading || page === 1}
+                                onClick={() => setPage(page - 1)}
+                            />{" "}
+                            <Text display={"inline-block"} marginX={"5"}>
+                                page {page}
+                            </Text>
+                            <IconButton
+                                aria-label="right-icon"
+                                icon={<FaAngleRight />}
+                                colorScheme={"blue"}
+                                variant={"ghost"}
+                                isDisabled={
+                                    isLoading || data !== undefined
+                                        ? data![data!.length - 1].height === 1
+                                        : true
+                                }
+                                onClick={() => setPage(page + 1)}
+                            />{" "}
+                        </TableCaption>
+                    </Table>
+                </TableContainer>
             </Box>
         </>
     );
